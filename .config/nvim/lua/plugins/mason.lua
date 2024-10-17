12fe8830c4633ad5e6ac49edff4e0a08fda65861
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-10-17 15:27:02 +0200

    docs(nvim): set lsp and telescope keymaps descriptions

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -59,21 +59,35 @@ return {
             callback = function(event)
                 local opts = {buffer = event.buf}
                 -- recommended
+                opts.desc = "show information"
                 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                opts.desc = "jumps to definition"
                 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                opts.desc = "jumps to declaration"
                 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                opts.desc = "list implementations"
                 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                opts.desc = "jumps to type definition"
                 vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                opts.desc = "list references"
                 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                opts.desc = "show signature information"
                 vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                opts.desc = "renames all references"
                 vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                opts.desc = "format using lsp client"
                 vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                opts.desc = "Select code action"
                 vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
                 -- defaults fix
+                opts.desc = "show diagnostic message"
                 vim.keymap.set('n', '<c-W>d', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+                opts.desc = "go to next diagnostic"
                 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+                opts.desc = "go to previous diagnostic"
                 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
                 -- custom
+                opts.desc = "show diagnostic message"
                 vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
             end,
         })
