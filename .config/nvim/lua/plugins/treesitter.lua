348d73f1e4895c4ff4770ee5d1d2a8868b2be05c
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2024-10-18 13:48:28 +0200

    feat(nvim): add nvim-treesitter-context plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -38,4 +38,12 @@ return {
             })
         end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {},
+        config = function()
+            vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true , sp = "Grey" })
+            vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true , sp = "Grey" })
+        end,
+    }
 }
