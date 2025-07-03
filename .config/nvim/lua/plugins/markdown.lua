82dd3f3aad7da2decf3a4241ce4c69eeff4209b5
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-07-03 17:51:05 +0200

    feat(nvim): add markdown-renderer

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,30 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        render_modes = true,
+        completions = {
+            lsp = {
+                enable = true,
+            },
+        },
+        code = {
+            width = 'block',
+            min_width = 80,
+            language_border = ' ',
+            language_left = '',
+            language_right = '',
+        },
+        dash = { 
+            width = 80,
+        },
+        heading = {
+            width = 'block',
+            min_width =  80,
+        },
+    },
+}
