c43bab1e816c2dafa8a5adcca889dc66b48717f6
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-09-21 06:34:22 +0200

    feat(nvim.plugins.languages.markdown): add otter

--- .config/nvim/lua/plugins/editor/languages/markdown.lua
+++ .config/nvim/lua/plugins/editor/languages/markdown.lua
@@ -1,29 +1,50 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ft = 'markdown',
-    opts = {
-        render_modes = true,
-        completions = {
-            lsp = {
-                enable = true,
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ft = 'markdown',
+        opts = {
+            render_modes = true,
+            completions = {
+                lsp = {
+                    enable = true,
+                },
+            },
+            code = {
+                width = 'block',
+                min_width = 80,
+                language_border = ' ',
+                language_left = '',
+                language_right = '',
+            },
+            dash = {
+                width = 80,
+            },
+            heading = {
+                width = 'block',
+                min_width = 80,
             },
         },
-        code = {
-            width = 'block',
-            min_width = 80,
-            language_border = ' ',
-            language_left = '',
-            language_right = '',
-        },
-        dash = { 
-            width = 80,
-        },
-        heading = {
-            width = 'block',
-            min_width =  80,
+    },
+    {
+        "jmbuhr/otter.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        ft = "markdown",
+        init = function()
+            vim.api.nvim_create_autocmd({ "BufRead", "InsertLeave" }, {
+                group = vim.api.nvim_create_augroup("otter-autostart", {}),
+                pattern = { "*.md" },
+                callback = function()
+                    require("otter").activate()
+                end,
+            })
+        end,
+        opts = {
+            lsp = {
+                diagnostic_update_events = { "BufWritePost", "InsertLeave", "TextChanged" },
+            },
         },
     },
 }
