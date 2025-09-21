f85b834ed3170d243fc0799cf246924361b8231a
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-09-21 06:24:46 +0200

    feat(nvim.plugins.lsp): rework lsp config to modern standards and add extra plugins
    Added:
    - Project wide diagnostics
    - Hover hints on mouse

--- .config/nvim/lua/plugins/editor/ide/lsp.lua
+++ .config/nvim/lua/plugins/editor/ide/lsp.lua
@@ -1,78 +1,108 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-        {
-            "mason-org/mason.nvim",
-            opts = {
-                registries = { "github:mason-org/mason-registry" },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            {
+                "mason-org/mason.nvim",
+                opts = {
+                    registries = { "github:mason-org/mason-registry" },
+                },
+            },
+            {
+                "neovim/nvim-lspconfig",
+                config = function()
+                    vim.lsp.config("lua_ls", {
+                        settings = {
+                            Lua = {
+                                diagnostics = { globals = { 'vim' }, },
+                                workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+                            },
+                        },
+                    })
+
+                    -- Create an autocommand group to prevent duplicate keymaps on reload
+                    vim.api.nvim_create_augroup("Lsp_Keymaps", { clear = true })
+
+                    -- Create the LspAttach autocommand
+                    vim.api.nvim_create_autocmd("LspAttach", {
+                        group = "Lsp_Keymaps",
+                        callback = function(args)
+                            -- Get the buffer number for the newly attached LSP client
+                            local bufnr = args.buf
+
+                            -- Set the keymaps for LSP actions on the specific buffer
+                            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Show information", buffer = bufnr })
+                            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Jump to definition", buffer = bufnr })
+                            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Jump to declaration", buffer = bufnr })
+                            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "List implementations", buffer = bufnr })
+                            vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { desc = "Jump to type definition", buffer = bufnr })
+                            vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "List references", buffer = bufnr })
+                            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = "Show signature information", buffer = bufnr })
+                            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename all references", buffer = bufnr })
+                            vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, { desc = "Rename all references", buffer = bufnr })
+                            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Select code action", buffer = bufnr })
+                            vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, { desc = "Select code action", buffer = bufnr })
+                            vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, { desc = "Format using LSP client", buffer = bufnr })
+                            vim.keymap.set({ 'n', 'x' }, '<leader>gf', function() vim.lsp.buf.format({ async = true }) end, { desc = "Format using LSP client", buffer = bufnr })
+
+                            -- Set keymaps for diagnostics
+                            vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Show diagnostic message", buffer = bufnr })
+                            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic", buffer = bufnr })
+                            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic", buffer = bufnr })
+                        end,
+                    })
+                end,
             },
         },
-        "neovim/nvim-lspconfig",
+        opts = {},
     },
-    init = function()
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
-
-        vim.diagnostic.config({
-            signs = true,
-            virtual_text = {
-                severity = vim.diagnostic.severity.ERROR
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        dependencies = {
+            {
+                "artemave/workspace-diagnostics.nvim",
+                opts = {}
             },
-            underline = {
-                severity = vim.diagnostic.severity.WARN
+        },
+        event = "VeryLazy",
+        priority = 1000,
+        init = function()
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN]  = " ",
+                        [vim.diagnostic.severity.INFO]  = "󰠠 ",
+                        [vim.diagnostic.severity.HINT]  = " ",
+                    },
+                },
+                virtual_text = false,
+                underline = {
+                    severity = {
+                        vim.diagnostic.severity.ERROR,
+                        vim.diagnostic.severity.WARN,
+                    },
+                },
+                severity_sort = true,
+            })
+        end,
+        opts = {
+            hi = {
+                background = "NonText",
             },
-            severity_sort = true,
-        })
-
-        -- Create the lsp keymaps only when a
-        -- language server is active
-        vim.api.nvim_create_autocmd('LspAttach', {
-            desc = 'LSP actions',
-            callback = function(event)
-                local opts = { buffer = event.buf }
-                -- recommended
-                opts.desc = "show information"
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                opts.desc = "jumps to definition"
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                opts.desc = "jumps to declaration"
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                opts.desc = "list implementations"
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                opts.desc = "jumps to type definition"
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                opts.desc = "list references"
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                opts.desc = "show signature information"
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                opts.desc = "renames all references"
-                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                opts.desc = "format using lsp client"
-                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                opts.desc = "Select code action"
-                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-                -- defaults fix
-                opts.desc = "show diagnostic message"
-                vim.keymap.set('n', '<c-W>d', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-                opts.desc = "go to next diagnostic"
-                vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-                opts.desc = "go to previous diagnostic"
-                vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-                -- custom
-                opts.desc = "show diagnostic message"
-                vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-            end,
-        })
-
-        local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-        function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-            opts = opts or {}
-            opts.border = opts.border or 'rounded'
-            return orig_util_open_floating_preview(contents, syntax, opts, ...)
-        end
-    end,
-    opts = {},
+            options = {
+                use_icons_from_diagnostic = true,
+                set_arrow_to_diag_color = true,
+            },
+        },
+    },
+    {
+        "soulis-1256/eagle.nvim",
+        init = function()
+            vim.o.mousemoveevent = true
+        end,
+        opts = {
+            --override the default values found in config.lua
+        }
+    },
 }
