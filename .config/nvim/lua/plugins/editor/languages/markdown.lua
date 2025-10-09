38c46ed1a551d00a82cbaacb8dc5d27580514205
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-10-09 10:18:10 +0200

    feat(nvim.plugins.editor.languages.markdown): add table-nvim

--- .config/nvim/lua/plugins/editor/languages/markdown.lua
+++ .config/nvim/lua/plugins/editor/languages/markdown.lua
@@ -29,6 +29,27 @@ return {
         },
     },
     {
+        'SCJangra/table-nvim',
+        ft = 'markdown',
+        opts = {
+            mappings = {
+                next = '<C-TAB>',                -- Go to next cell.
+                prev = '<C-S-TAB>',              -- Go to previous cell.
+                insert_row_up = '<C-A-k>',       -- Insert a row above the current row.
+                insert_row_down = '<C-A-j>',     -- Insert a row below the current row.
+                move_row_up = '<A-S-k>',         -- Move the current row up.
+                move_row_down = '<A-S-j>',       -- Move the current row down.
+                insert_column_left = '<C-A-h>',  -- Insert a column to the left of current column.
+                insert_column_right = '<C-A-l>', -- Insert a column to the right of current column.
+                move_column_left = '<A-S-h>',    -- Move the current column to the left.
+                move_column_right = '<A-S-l>',   -- Move the current column to the right.
+                insert_table = '<A-t>',          -- Insert a new table.
+                insert_table_alt = '<A-S-t>',    -- Insert a new table that is not surrounded by pipes.
+                delete_column = '<A-d>',         -- Delete the column under cursor.
+            }
+        },
+    },
+    {
         "jmbuhr/otter.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         ft = "markdown",
