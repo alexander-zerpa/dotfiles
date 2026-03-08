01203716ae30dc98cb6123ad1cde7627105dc2b7
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2026-03-08 16:39:57 +0100

    feat(nvim): enable listchars

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -33,6 +33,16 @@ local options = {
     winborder = 'rounded',
 
     showmode = false,
+
+    list = true,
+    listchars = {
+        tab = '» ',       -- Show tabs as '» '
+        trail = '·',      -- Show trailing spaces as dots
+        nbsp = '␣',       -- Show non-breaking spaces
+        extends = '›',    -- Character to show in the last column when wrap is off
+        precedes = '‹',   -- Character to show in the first column when wrap is off
+        -- eol = '↲',        -- Optional: Show a character at the end of each line
+    }
 }
 
 for k, v in pairs(options) do
